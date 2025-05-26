5866b8526962e502215527e3ae18a4499aaecbc7
Roberto Landa <robertolanda@Landa-2.local>
2025-05-26 18:44:05 -0500

    add accounts2

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,42 +2,13 @@ module "sandbox1" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "sebastianleonardo299+aft02@gmail.com"
-    AccountName               = "sandbox-ST02"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "sebastianleonardo299+aft@gmail.com"
-    SSOUserFirstName          = "Sandbox6" 
-    SSOUserLastName           = "AFT6"
-  }
-
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Batch account creation 2/2"
-  }
-
-  custom_fields = {
-    group = "non-prod"
-  }
-
-  account_customizations_name = "sandbox"
-}
-
-
-module "sandbox2" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "rlanda+wptest@morrisopazo.com"
-    AccountName               = "sandbox-ST02"
+    AccountEmail              = "rlanda+1@morrisopazo.com"
+    AccountName               = "sandbox-aft1"
     ManagedOrganizationalUnit = "Learn AFT"
     SSOUserEmail              = "rlanda+wp@morrisopazo.com"
-    SSOUserFirstName          = "Sandbox7" 
-    SSOUserLastName           = "AFT7"
-  }
+    SSOUserFirstName          = "RL1"
+    SSOUserLastName           = "AFT"
+   }
 
   account_tags = {
     "Learn Tutorial" = "AFT",
@@ -46,8 +17,35 @@ module "sandbox2" {
 
   change_management_parameters = {
     change_requested_by = "HashiCorp Learn"
+    change_reason       = "Batch account creation 1/2"
+  }
+
+  account_customizations_name = "sandbox"
+}
+
+module "sandbox2" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "sebastianleonardo299+aft03@gmail.com"
+    AccountName               = "sandbox-ST03"
+    ManagedOrganizationalUnit = "Learn AFT"
+    SSOUserEmail              = "sebastianleonardo299+aft@gmail.com"
+    SSOUserFirstName          = "Sandbox8" 
+    SSOUserLastName           = "AFT8"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT",
+    "Environment"    = "Sandbox",
+    "Owner"          = "Team-DevOps"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
     change_reason       = "Batch account creation 2/2"
   }
 
   account_customizations_name = "sandbox"
+
 }
