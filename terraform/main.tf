77e1b5e1e7691cecdb8fd5b7f562cd00fe452820
Roberto Landa <robertolanda@Landa-2.local>
2025-05-26 18:13:51 -0500

    add accounts

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,45 +1,47 @@
-# main.tf
-module "sandbox_3" {
+module "sandbox1" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "sebastianleonardo299+aft@gmail.com"
-    AccountName               = "sandbox-ST"
+    AccountEmail              = "sebastianleonardo299+aft02@gmail.com"
+    AccountName               = "sandbox-ST02"
     ManagedOrganizationalUnit = "Learn AFT"
     SSOUserEmail              = "sebastianleonardo299+aft@gmail.com"
-    SSOUserFirstName          = "Sandbox5" 
-    SSOUserLastName           = "AFT5"
-  }
-
-  account_tags = {
-    "Learn Tutorial" = "AFT",
-    "Environment"    = "Sandbox"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Batch account creation 1/2"
-  }
-
-  account_customizations_name = "sandbox"
-}
-
-module "sandbox_4" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "ifcatalan98+aft@gmail.com"
-    AccountName               = "sandbox-IC"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "ifcatalan98+aft@gmail.com"
     SSOUserFirstName          = "Sandbox6" 
     SSOUserLastName           = "AFT6"
   }
 
   account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Batch account creation 2/2"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+
+
+module "sandbox2" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "rlanda+wptest@morrisopazo.com"
+    AccountName               = "sandbox-ST02"
+    ManagedOrganizationalUnit = "Learn AFT"
+    SSOUserEmail              = "rlanda+wp@morrisopazo.com"
+    SSOUserFirstName          = "Sandbox7" 
+    SSOUserLastName           = "AFT7"
+  }
+
+  account_tags = {
     "Learn Tutorial" = "AFT",
-    "Environment"    = "Sandbox",
-    "Owner"          = "Team-DevOps"
+    "Environment"    = "Sandbox"
   }
 
   change_management_parameters = {
