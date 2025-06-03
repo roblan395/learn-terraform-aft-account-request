ef38ad3aa74c7fc5aaeeaeccccd8b57dee6fe90e
Roberto Landa <robertolanda@Landa-2.local>
2025-06-03 15:31:48 -0500

    Test de1

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,23 +1,26 @@
-module "test_account" {
+module "dormant_account" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "lab-account-1@test.com"
-    AccountName               = "lab-account-1"
+    AccountEmail              = "rlanda-de1@morrisopazo.com"
+    AccountName               = "lab-account-2"
     ManagedOrganizationalUnit = "Workshop Serverless DE"
-    SSOUserEmail              = "lab-account-1@test.com"
-    SSOUserFirstName          = "Test"
-    SSOUserLastName           = "lab-account-1"
+    # Deja estos campos vacíos para evitar SSO automático
+    SSOUserEmail              = ""
+    SSOUserFirstName          = ""
+    SSOUserLastName           = ""
   }
 
   account_tags = {
-    "Environment" = "Test",
-    "Owner"       = "rlanda"
+    "Environment"    = "Dormant",
+    "Owner"          = "rlanda",
+    "AccountStatus"  = "PendingActivation",  # Importante para el filtrado
+    "ActivationType" = "OTP"
   }
 
   change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Test account with custom resources"
+    change_requested_by = "OTP Activation System",
+    change_reason       = "Dormant account for OTP activation"
   }
 
   account_customizations_name = "sandbox"
